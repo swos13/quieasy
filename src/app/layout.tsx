2b007b4656f97181c2a94edb1f09f13793174208
Szymon Wos <s.wos@viseven.com>
2025-05-15 15:36:33 +0200

    setup project

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Roboto } from "next/font/google";
+import { ThemeProvider } from "@mui/material";
+import "./globals.css";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+
+import theme from "./theme";
+
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable}`}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
