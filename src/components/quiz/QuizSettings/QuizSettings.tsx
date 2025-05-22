65112d98df6b9ab4574c500fcf699dd967db1619
Szymon <szymon.wos1319@gmail.com>
2025-05-22 10:58:29 +0200

    design - add settings options

--- src/components/quiz/QuizSettings/QuizSettings.tsx
+++ src/components/quiz/QuizSettings/QuizSettings.tsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import { createSearchParams } from "../../../tools/helpers";
 import { Difficulty, QuestionType } from "@/app/lib/types";
 import style from "./QuizSettings.module.css";
-import { Button, InputLabel, Select } from "@mui/material";
+import { Button, InputLabel, MenuItem, Select } from "@mui/material";
+import {amounts, categories, difficulties, questionTypes} from "./optionsValues";
 
 interface SearchProps {
     amount: number,
@@ -40,20 +41,28 @@ export default function QuizSettings() {
   return (
     <div className={style.container}>
       <div className={style.option}>
-        <InputLabel id="amount-select-label">Number of questions:</InputLabel>
-        <Select labelId="amount-select-label" value={searchProps.amount} onChange={(event) => handleUpdate("amount", event.target.value)}></Select>
+        <InputLabel className={style.option_label} id="amount-select-label">Number of questions:</InputLabel>
+        <Select labelId="amount-select-label" value={searchProps.amount} onChange={(event) => handleUpdate("amount", event.target.value)}>
+            {amounts.map((amount) => <MenuItem value={amount} key={amount}>{amount}</MenuItem>)}
+        </Select>
       </div>
       <div className={style.option}>
-        <InputLabel id="category-select-label">Category of questions:</InputLabel>
-        <Select labelId="category-select-label" value={searchProps.category} onChange={(event) => handleUpdate("category", event.target.value)}></Select>
+        <InputLabel className={style.option_label} id="category-select-label">Category of questions:</InputLabel>
+        <Select labelId="category-select-label" value={searchProps.category} onChange={(event) => handleUpdate("category", event.target.value)}>
+            {categories.map((category) => <MenuItem value={category.value} key={category.value}>{category.name}</MenuItem>)}
+        </Select>
       </div>
       <div className={style.option}>
-        <InputLabel id="type-select-label">Type:</InputLabel>
-        <Select labelId="type-select-label" value={searchProps.type} onChange={(event) => handleUpdate("type", event.target.value)}></Select>
+        <InputLabel className={style.option_label} id="type-select-label">Type:</InputLabel>
+        <Select labelId="type-select-label" value={searchProps.type} onChange={(event) => handleUpdate("type", event.target.value)}>
+            {questionTypes.map((type, index) => <MenuItem value={type.value} key={index}>{type.name}</MenuItem>)}
+        </Select>
       </div>
       <div className={style.option}>
-        <InputLabel id="difficulty-select-label">Difficulty:</InputLabel>
-        <Select labelId="difficulty-select-label" value={searchProps.difficulty} onChange={(event) => handleUpdate("difficulty", event.target.value)}></Select>
+        <InputLabel className={style.option_label} id="difficulty-select-label">Difficulty:</InputLabel>
+        <Select labelId="difficulty-select-label" value={searchProps.difficulty} onChange={(event) => handleUpdate("difficulty", event.target.value)}>
+            {difficulties.map((difficulty, index) => <MenuItem value={difficulty.value} key={index}>{difficulty.name}</MenuItem>)}
+        </Select>
       </div>
       <Button onClick={handleSubmit}>Submit</Button>
     </div>
---
+++ src/components/quiz/QuizSettings/optionsValues.tsx
@@ -0,0 +1,47 @@
+export const amounts = [10, 20, 30];
+
+export const categories = [
+  { value: 0, name: "Any category (mix)" },
+  { value: 9, name: "General Knowledge" },
+
+  { value: 17, name: "Science & Nature" },
+  { value: 18, name: "Computers" },
+  { value: 19, name: "Mathematics" },
+  { value: 20, name: "Mythology" },
+  { value: 21, name: "Sports" },
+  { value: 22, name: "Geography" },
+  { value: 23, name: "History" },
+
+  { value: 27, name: "Animals" },
+  { value: 24, name: "Politics" },
+
+  { value: 25, name: "Art" },
+  { value: 10, name: "Books" },
+  { value: 11, name: "Film" },
+  { value: 12, name: "Music" },
+  { value: 13, name: "Musical & Theatres" },
+  { value: 14, name: "Television" },
+  { value: 26, name: "Celebrities" },
+  { value: 15, name: "Video Games" },
+  { value: 16, name: "Board Games" },
+
+  { value: 28, name: "Vehicles" },
+  { value: 30, name: "Gadgets" },
+
+  { value: 29, name: "Comics" },
+  { value: 32, name: "Cartoon & Animations" },
+  { value: 31, name: "Japanese Manga & Anime" },
+];
+
+export const difficulties = [
+  { value: "", name: "Any difficulty" },
+  { value: "easy", name: "Easy" },
+  { value: "medium", name: "Medium" },
+  { value: "hard", name: "Hard" },
+];
+
+export const questionTypes = [
+  { value: "", name: "Any" },
+  { value: "multiple", name: "Multiple choice" },
+  { value: "boolean", name: "True/False" },
+];
